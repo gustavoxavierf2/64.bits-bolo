491671a8b7100437d2266863e28abca57b30b3bb
gustavo x <guxfarias7@gmail.com>
2024-02-06 13:29:13 -0300

    update 3, add ajax na div de conteudo e links

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,3 @@
-
-// const tabs = document.querySelectorAll('.tab-btn');
-
-// tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
-
-// const tabClicked = (tab) => {
-//     tabs.forEach(tab => tab.classList.remove('active'));
-//     tab.classList.add('active');
-
-//     const contents = document.querySelectorAll('.content');
-//     contents.forEach(content => content.classList.remove('show'));
-
-//     const contentId = tab.getAttribute('content-id');
-//     const content = document.getElementById(contentId);
-//     console.log(contentId)
-//     console.log(content)
-
-//     content.classList.add('show');
-// }
-
-// const currentActiveTab = document.querySelector('.tab-btn.active');
-// tabClicked(currentActiveTab);
-
 // Função para construir a URL com a mensagem citada e abrir no navegador
 function abrirWhatsApp() {
     var numeroTelefone = '';
